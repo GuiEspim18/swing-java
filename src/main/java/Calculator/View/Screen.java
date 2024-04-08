1fad2e85a95fc3e16b51e158ad959b536b52988b
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2024-04-08 11:23:20 -0300

    class created

---
+++ src/main/java/Calculator/View/Screen.java
@@ -0,0 +1,9 @@
+package Calculator.View;
+
+public class Screen {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/main/java/Calculator/main/Main.java
@@ -0,0 +1,9 @@
+package Calculator.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
