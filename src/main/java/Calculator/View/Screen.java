575bf81ec51c8763667c9422112b5e3e9a789677
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-04-12 11:20:32 -0300

    calculator

--- src/main/java/Calculator/View/Screen.java
+++ src/main/java/Calculator/View/Screen.java
@@ -5,26 +5,62 @@ import javax.swing.JOptionPane;
 public class Screen {
 
 	public static void show() {
-		String message = "1. Soma\n2. Subtração\n3. Multiplicação\n4. Divisão";
-		int option = Integer.parseInt(JOptionPane.showInputDialog(null, message));
-		switch (option) {
-			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			default:
-				break;
-		
+		boolean exit = false;
+		while (!exit) {
+			String message = "1. Soma\n2. Subtração\n3. Multiplicação\n4. Divisão\n5. Sair";
+			int option = Integer.parseInt(JOptionPane.showInputDialog(null, message));
+			switch (option) {
+				case 1:
+					sum(getValues());
+					break;
+				case 2:
+					subtraction(getValues());
+					break;
+				case 3:
+					multiplication(getValues());
+					break;
+				case 4:
+					division(getValues());
+					break;
+				case 5:
+					exit = true;
+					break;
+				default:
+					JOptionPane.showMessageDialog(null, "Erro, opção inválida!");
+					break;
+			}
+
 		}
 	}
 	
-	private int[] getValues() {
+	private static int[] getValues() {
 		int[] values = new int[2];
+		values[0] = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um valor"));
+		values[1] = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite mais um valor"));
 		return values;
 	}
-	
+
+	private static void sum(int[] values) {
+		int result = values[0] + values[1];
+		String message = values[0] + " + " + values[1] + " = " + result;
+		JOptionPane.showMessageDialog(null, message);
+	}
+
+	private static void subtraction(int[] values) {
+		int result = values[0] - values[1];
+		String message = values[0] + " - " + values[1] + " = " + result;
+		JOptionPane.showMessageDialog(null, message);
+	}
+
+	private static void multiplication(int[] values) {
+		int result = values[0] * values[1];
+		String message = values[0] + " x " + values[1] + " = " + result;
+		JOptionPane.showMessageDialog(null, message);
+	}
+
+	private static void division(int[] values) {
+		int result = values[0] / values[1];
+		String message = values[0] + " / " + values[1] + " = " + result;
+		JOptionPane.showMessageDialog(null, message);
+	}
 }
