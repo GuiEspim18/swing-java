87277366e86199d19c8a16cd368aff53e3f7ac10
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-05-01 22:25:24 -0300

    add flight class

---
+++ src/main/java/Airline/Main/Main.java
@@ -0,0 +1,4 @@
+package Airline.Main;
+
+public class Main {
+}
---
+++ src/main/java/Airline/Model/Flight.java
@@ -0,0 +1,9 @@
+package Airline.Model;
+
+public class Flight {
+
+    public String destination;
+    public String origin;
+    public int duration;
+
+}
